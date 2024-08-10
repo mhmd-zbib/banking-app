d10d20bdb05498bfd9203cd0e68786775d14d7de
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-08-11 00:03:36 +0300

    init

---
+++ src/main/java/dev/bank/bankingapp/BankingAppApplication.java
@@ -0,0 +1,14 @@
+package dev.bank.bankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/dev/bank/bankingapp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bank.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
