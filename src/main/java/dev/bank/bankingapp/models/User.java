9e2bb311763118b3710d5a51ed662b97184881c4
mhmd-zbib <mohammad.h.zbib@gmail.com>
2024-08-11 18:34:08 +0300

    [FEAT] 001 - Add user class

--- src/main/java/dev/bank/bankingapp/models/User.java
+++ src/main/java/dev/bank/bankingapp/models/User.java
@@ -1,2 +1,26 @@
-package dev.bank.bankingapp.models;public class User {
+package dev.bank.bankingapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+    private boolean isEmailVerified = false;
+    private boolean isAccountVerified = false;
+
+
+
 }
